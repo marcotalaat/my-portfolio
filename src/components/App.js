5ae9fdfbd02f5c0a44e057e43dbfbf3ea3ececf3
Marco <64090277+marcotalaat@users.noreply.github.com>
2023-09-03 22:13:16 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-import Navbar from "./Navbar/NavbarDesktop/navbar";
-import Landing from "./landing/landing";
-import About from "./About/about";
-import Choose from "./About/choose-me";
-import Works from "./Works/works";
-import Reviews from "./reviews/reviews";
-import Contact from "./contact/contact";
-import { Footer } from "./Footer/footer";
-import NavMobile from "./Navbar/NavbarMobile/navMobile";
-import AboutMobile from "./About/aboutMobile";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import Admin from "./Admin/admin";
-
 function App() {
   const state = {
     cv: { id: 1, count: 0 },
@@ -38,24 +24,10 @@ function App() {
     });
   }, []);
 
-  const showNav = nav ? <NavMobile /> : <Navbar />;
-  const showAbout = about ? <AboutMobile /> : <About />;
-
-  const showAdmin =
-    window.location.href === "http://localhost:3000/admin3391" ? <Admin /> : null;
 
   return (
     <>
-      {showAdmin}
-      {showNav}
-
-      <Landing countCv={state.cv} />
-      {showAbout}
-      <Choose />
-      <Works />
-      <Reviews />
-      <Contact />
-      <Footer />
+      <h1>Welcome</h1>
     </>
   );
 }
