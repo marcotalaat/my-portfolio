121f69618db8efeffd5e25c5f18fe1f0f6289d55
Marco <64090277+marcotalaat@users.noreply.github.com>
2023-09-03 20:49:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
+import Navbar from "./components/Navbar/NavbarDesktop/navbar";
+import Landing from "./components/landing/landing";
+import About from "./components/About/about";
+import Choose from "./components/About/choose-me";
+import Works from "./components/Works/works";
+import Reviews from "./components/reviews/reviews";
+import Contact from "./components/contact/contact";
+import { Footer } from "./components/Footer/footer";
+import NavMobile from "./components/Navbar/NavbarMobile/navMobile";
+import AboutMobile from "./components/About/aboutMobile";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Admin from "./components/Admin/admin";
 function App() {
   return (
     <div className="App">
