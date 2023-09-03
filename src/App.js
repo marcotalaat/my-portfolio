8c7c1e875d5edeb09e1f50de20005fad9eb2f50d
marcotalaat1412 <marcotalaat1412@githup.com>
2023-09-03 20:44:07 +0300

    app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "animate.css/animate.min.css";
+
+import Navbar from "./components/Navbar/NavbarDesktop/navbar";
+import Landing from "./components/landing/landing";
+import About from "./components/About/about";
+import Choose from "./components/About/choose-me";
+import Works from "./components/Works/works";
+import Reviews from "./components/reviews/reviews";
+import Contact from "./components/contact/contact";
+import { Footer } from "./components/Footer/footer";
+import NavMobile from "./components/Navbar/NavbarMobile/navMobile";
+import AboutMobile from "./components/About/aboutMobile";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Admin from "./components/Admin/admin";
 
 function App() {
+  const state = {
+    cv: { id: 1, count: 0 },
+  };
+  AOS.init({
+    duration: 500,
+  });
+
+  const [nav, setNav] = useState(true);
+  const [about, setAbout] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("load", function () {
+      if (this.innerWidth <= 1000) {
+        setNav(true);
+        setAbout(true);
+      } else {
+        setNav(false);
+        setAbout(false);
+      }
+    });
+  }, []);
+
+  const showNav = nav ? <NavMobile /> : <Navbar />;
+  const showAbout = about ? <AboutMobile /> : <About />;
+
+  const showAdmin =
+    window.location.href === "http://localhost:3000/admin3391" ? <Admin /> : null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {showAdmin}
+      {showNav}
+
+      <Landing countCv={state.cv} />
+      {showAbout}
+      <Choose />
+      <Works />
+      <Reviews />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
