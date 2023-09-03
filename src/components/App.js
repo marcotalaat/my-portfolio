4996f6918a194c977852c0f9335df4ea155138f1
Marco <64090277+marcotalaat@users.noreply.github.com>
2023-09-03 22:25:08 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
+import "./app.css";
 
 import Navbar from "./components/Navbar/NavbarDesktop/navbar";
 import Landing from "./components/landing/landing";
