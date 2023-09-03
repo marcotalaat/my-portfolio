afe4dac7e0754ec0daa01b4c5a8571f35648ceea
Marco <64090277+marcotalaat@users.noreply.github.com>
2023-09-03 22:23:53 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,20 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
+import Navbar from "./components/Navbar/NavbarDesktop/navbar";
+import Landing from "./components/landing/landing";
+import About from "./components/About/about";
+import Choose from "./components/About/choose-me";
+import Works from "./components/Works/works";
+import Reviews from "./components/reviews/reviews";
+import Contact from "./components/contact/contact";
+import { Footer } from "./components/Footer/footer";
+import NavMobile from "./components/Navbar/NavbarMobile/navMobile";
+import AboutMobile from "./components/About/aboutMobile";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import Admin from "./components/Admin/admin";
+
 function App() {
   const state = {
     cv: { id: 1, count: 0 },
@@ -24,10 +38,24 @@ function App() {
     });
   }, []);
 
+  const showNav = nav ? <NavMobile /> : <Navbar />;
+  const showAbout = about ? <AboutMobile /> : <About />;
+
+  const showAdmin =
+    window.location.href === "http://localhost:3000/admin3391" ? <Admin /> : null;
 
   return (
     <>
-      <h1>Welcome</h1>
+      {showAdmin}
+      {showNav}
+
+      <Landing countCv={state.cv} />
+      {showAbout}
+      <Choose />
+      <Works />
+      <Reviews />
+      <Contact />
+      <Footer />
     </>
   );
 }
