1685deb520c17e0f3fa7fb73dd799b8af59478fc
marcotalaat1412 <marcotalaat1412@githup.com>
2023-09-04 23:49:53 +0300

    addContactForm

--- src/components/contact/form.jsx
+++ src/components/contact/form.jsx
@@ -1,32 +1,65 @@
-import React from 'react'
+import React, { useRef } from "react";
 import Input from "./input";
 import "./contact.css";
+import emailjs from "@emailjs/browser";
 
 export const Form = () => {
-  const submit = () => {
-    setTimeout(() => {
-      alert("Thank you for your message!");
-    }, 1000)
-  }
+
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_vagt0pn', 'template_alfunel', e.target, 'olmh400L2LdpnRI3v')
+      .then((result) => {
+        setTimeout(() => {
+          alert("Thank you for your message, I will contact with you soon!");
+        }, 1000);
+      }, (error) => {
+        setTimeout(() => {
+          alert("Error occure. try again, please.");
+        }, 1000);
+      });
+  };
+
   return (
-    <form action="mailto:marcotalaat355@yahoo.com" method="POST" onSubmit={submit} encType='text/plain'>
+    <form
+      action="mailto:marcotalaat355@yahoo.com"
+      method="POST"
+      ref={form}
+      onSubmit={sendEmail}
+      encType="text/plain"
+    >
       <div className="row">
         <div className="col-md-6">
-          <Input type={"text"} label={"First Name"} attr={"first-name"} name={"first_name"} />
+          <Input
+            type={"text"}
+            label={"First Name"}
+            attr={"first-name"}
+            name={"first_name"}
+          />
         </div>
         <div className="col-md-6">
-          <Input type={"text"} label={"Last Name"} attr={"last-name"} name={"last_name"} />
+          <Input
+            type={"text"}
+            label={"Last Name"}
+            attr={"last-name"}
+            name={"last_name"}
+          />
         </div>
       </div>
-        <Input type={"email"} label={"E-mail"} attr={"email"} name={"email"} />
-        <Input type={"text"} label={"Subject"} attr={"subject"} name={"subject"} />
+      <Input type={"email"} label={"E-mail"} attr={"email"} name={"email"} />
+      <Input
+        type={"text"}
+        label={"Subject"}
+        attr={"subject"}
+        name={"subject"}
+      />
 
-        <div className='field-wrapper'>
-            <label htmlFor="msg" >Your Message</label>
-            <textarea name="msg" id="msg" className="msg"></textarea>
-        </div>
+      <div className="field-wrapper">
+        <label htmlFor="msg">Your Message</label>
+        <textarea name="msg" id="msg" className="msg"></textarea>
+      </div>
 
-        <input type="submit" value="Send Message" className="submit"/>
+      <input type="submit" value="Send Message" className="submit" />
     </form>
   );
 };
